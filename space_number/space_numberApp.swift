7c43a9f950d017611922fcc6dce70f3006e294a9
restacksyj <restacksyj@gmail.com>
2024-11-15 01:16:36 -0500

    Initial Commit

---
+++ space_number/space_numberApp.swift
@@ -0,0 +1,17 @@
+//
+//  space_numberApp.swift
+//  space_number
+//
+//  Created by Yash Jajoo on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct space_numberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
